7746c5dd631c3f52fa4914552573e7540e195f28
Francesco Mariotti <francesco.mariotti01@gmail.com>
2025-10-20 22:05:25 +0200

    Backend - file .env spostato fuori da flask_app
    .env rinominato come flask_app_config.env

--- backend/flask_app/run.py
+++ backend/flask_app/run.py
@@ -1,12 +1,18 @@
+from flask import config
 from app import create_app
 from dotenv import load_dotenv
 from waitress import serve
 from pathlib import Path
-import os
 
-this_dir = Path(__file__).resolve().parent
+prev_dir = Path(__file__).resolve().parent.parent
+config_file_name = 'flask_app_config.env'
 
-load_dotenv(this_dir / '.env')
+config_loaded = load_dotenv(prev_dir / config_file_name)
+
+if not config_loaded:
+    print("Attenzione il file: " + config_file_name + " non e' stato trovato o non e' leggibile!!")
+    print("IMPOSSIBILE AVVIARE L' APPLICAZIONE")
+    exit(1)
 
 app = create_app()
 
